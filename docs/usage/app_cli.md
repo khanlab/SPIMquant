2888f5c49f71b96838fc98a73393ab79caf556e9
Ali Khan <alik@robarts.ca>
2024-02-15 16:44:26 -0500

    initial import

---
+++ docs/usage/app_cli.md
@@ -0,0 +1,7 @@
+## Command line interface
+
+```{argparse}
+:filename: ../spimquant/run.py
+:func: get_parser
+:prog: spimquant
+```
---
+++ spimquant/run.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+from pathlib import Path
+
+from snakebids.app import SnakeBidsApp
+from snakebids.plugins.validator import BidsValidator
+
+
+def get_parser():
+    """Exposes parser for sphinx doc generation, cwd is the docs dir"""
+    app = SnakeBidsApp("../spimquant")
+    return app.parser
+
+
+def main():
+    app = SnakeBidsApp(
+        Path(__file__).resolve().parent,  # to get repository root
+        plugins=[BidsValidator()],
+    )
+    app.run_snakemake()
+
+
+if __name__ == "__main__":
+    main()
