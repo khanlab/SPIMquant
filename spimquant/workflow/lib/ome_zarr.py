2c78cf2edbb219606efdb811f5af80200cc0c72f
Ali Khan <alik@robarts.ca>
2024-09-24 14:32:25 -0400

    change method for gettings channel names from metadata
    was using ome-zarr, so was not compatible with ome.zarr.zip

--- spimquant/workflow/lib/ome_zarr.py
+++ spimquant/workflow/lib/ome_zarr.py
@@ -1,37 +1,14 @@
-import ome_zarr
-from ome_zarr.io import parse_url
-from ome_zarr.reader import Reader
-
-
-def get_metadata(path):
-
-    # Parse the URL and load the OME-Zarr file
-    store = parse_url(path)
-    reader = Reader(store)
-    nodes = list(reader())
-
-    # Assume the first node is the image you want to read metadata from
-    image_node = nodes[0]
-
-    # Access the metadata
-    metadata = image_node.metadata
-    return metadata 
+import zarr
 
 def get_channel_names(path):
 
-    # Parse the URL and load the OME-Zarr file
-    store = parse_url(path)
-    reader = Reader(store)
-    nodes = list(reader())
+    group = zarr.open_group(path, mode='r')
 
-    # Assume the first node is the image you want to read metadata from
-    image_node = nodes[0]
-
-    # Access the metadata
-    metadata = image_node.metadata
+    # Access the .zattrs metadata as a dictionary
+    metadata = dict(group.attrs)
 
     # Retrieve channel names
-    channel_names = metadata['channel_names']
+    return [chan['label'] for chan in metadata['multiscales'][0]['metadata']['omero']['channels']]
 
     return channel_names
 
