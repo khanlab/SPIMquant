0451bae77dcb3d3755b9240650a005939bbab92e
Ali Khan <alik@robarts.ca>
2025-11-25 01:23:17 -0500

    update deps and fmt

--- spimquant/workflow/scripts/convert_zarr_to_ozx.py
+++ spimquant/workflow/scripts/convert_zarr_to_ozx.py
@@ -1,14 +1,12 @@
-#import ngff_zarr as nz
+# import ngff_zarr as nz
 from zarrnii import ZarrNii
 
 # Read from directory store
-#multiscales = nz.from_ngff_zarr(snakemake.input.zarr)
+# multiscales = nz.from_ngff_zarr(snakemake.input.zarr)
 
-#print(multiscales)
+# print(multiscales)
 # Write as .ozx file
-#nz.to_ngff_zarr(snakemake.output.ozx, multiscales, version='0.5')
+# nz.to_ngff_zarr(snakemake.output.ozx, multiscales, version='0.5')
 
-#note, this recomputes the multiscales
+# note, this recomputes the multiscales
 ZarrNii.from_ome_zarr(snakemake.input.zarr).to_ome_zarr(snakemake.output.ozx)
-
-
