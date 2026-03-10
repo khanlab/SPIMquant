f8a4925d398fe2f0509ae79ba6a9a1ee2864f105
Ali Khan <akhanf@users.noreply.github.com>
2026-03-10 09:43:27 -0400

    Use Dask client for loading image and data
    Refactored code to use Dask client for parallel processing and improved structure.

--- spimquant/workflow/scripts/counts_per_voxel.py
+++ spimquant/workflow/scripts/counts_per_voxel.py
@@ -6,16 +6,17 @@ import pandas as pd
 level = int(snakemake.wildcards.level)
 stain = snakemake.wildcards.stain
 
-img = ZarrNii.from_ome_zarr(
-    snakemake.input.ref_spim,
-    level=level,
-    channel_labels=[stain],
-    downsample_near_isotropic=True,
-)
-
-df = pd.read_parquet(snakemake.input.regionprops_parquet)
-
-points = df[snakemake.params.coord_column_names].values
+with get_dask_client("threads", snakemake.threads):
+    img = ZarrNii.from_ome_zarr(
+        snakemake.input.ref_spim,
+        level=level,
+        channel_labels=[stain],
+        downsample_near_isotropic=True,
+    )
+    
+    df = pd.read_parquet(snakemake.input.regionprops_parquet)
+    
+    points = df[snakemake.params.coord_column_names].values
 
 # Create counts map (zarrnii is calling this density right now)..
 counts = density_from_points(points, img, in_physical_space=True)
