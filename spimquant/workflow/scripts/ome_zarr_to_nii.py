9362ba0dfaa5cd60012ca069d5f4cc1644317bd5
Ali Khan <akhanf@users.noreply.github.com>
2026-03-10 09:45:46 -0400

    Refactor indentation for main execution block

--- spimquant/workflow/scripts/ome_zarr_to_nii.py
+++ spimquant/workflow/scripts/ome_zarr_to_nii.py
@@ -2,15 +2,14 @@ from dask.diagnostics import ProgressBar
 from dask_setup import get_dask_client
 from zarrnii import ZarrNii
 
-if __name__ == "__main__":
-    with get_dask_client("threads", snakemake.threads):
-        znimg = ZarrNii.from_ome_zarr(
-            snakemake.input.spim,
-            level=int(snakemake.wildcards.level),
-            channel_labels=[snakemake.wildcards.stain],
-            downsample_near_isotropic=True,
-            **snakemake.params.zarrnii_kwargs,
-        )
+with get_dask_client("threads", snakemake.threads):
+    znimg = ZarrNii.from_ome_zarr(
+        snakemake.input.spim,
+        level=int(snakemake.wildcards.level),
+        channel_labels=[snakemake.wildcards.stain],
+        downsample_near_isotropic=True,
+        **snakemake.params.zarrnii_kwargs,
+    )
 
-        with ProgressBar():
-            znimg.to_nifti(snakemake.output.nii)
+    with ProgressBar():
+        znimg.to_nifti(snakemake.output.nii)
